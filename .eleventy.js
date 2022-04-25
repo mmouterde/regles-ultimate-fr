bff892abbc969f0e2c68495508c48b602b90e72d
mmouterde <martin.mouterde@gmail.com>
2022-04-25 11:15:49 +0200

    use 11y to template and use json data

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,23 @@
+const MarkdownIt = require("markdown-it");
+const markdownIt = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true
+});
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/*.css");
+    eleventyConfig.addNunjucksFilter("markdown", function(value) {
+        return markdownIt.render(value); });
+    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+        if( outputPath && outputPath.endsWith(".html") ) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
+        return content;
+    });
 };
